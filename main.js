641046d569436fc1c1852afcfe78366403075db1
SURU EMMANUEL <suruaino@gmail.com>
2024-04-02 00:17:40 +0100

    clean up

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const searchBtn = document.querySelector('.search-btn');
 const searchInputContainer = document.querySelector('.search-input-container');
 const searchInput = document.querySelector('.search-input');
 
-// console.log(searchBtn)
 
-//a click on the search button should change the width of the search bar container.
 searchBtn.addEventListener('click', () => {
     searchBarContainer.style.width = "100%";
+    searchBarContainer.style.borderRadius = "10px";
     searchInputContainer.style.display = "block"
     searchInput.style.display = 'block';
     searchInput.style.width = '100%';
@@ -15,7 +14,8 @@ searchBtn.addEventListener('click', () => {
 
 document.body.addEventListener('click', (event) => {
     if (event.target !== searchBtn && !searchBarContainer.contains(event.target)) {
-        searchBarContainer.style.width = '1rem'; // Shrink width back to 100px
+        searchBarContainer.style.width = '1rem';
+        searchBarContainer.style.borderRadius = "50%";
         searchBarContainer.style.justifyContent = "center";
         searchInput.style.display = 'none';
         searchInputContainer.style.display = 'none';
